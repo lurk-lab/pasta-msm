58dfd5bef57f18d797053a2e61ced9c312626fc7
Hanting Zhang <winston@lurk-lab.com>
2024-01-12 23:31:56 +0000

    more stats

--- examples/abomonated_msm.rs
+++ examples/abomonated_msm.rs
@@ -1,4 +1,4 @@
-use std::{io::Read, time::Instant, collections::HashMap};
+use std::{io::{Read, BufWriter, Write}, time::Instant, collections::HashMap};
 
 use abomonation::Abomonation;
 use num::{BigInt, Num, Zero};
@@ -9,7 +9,9 @@ use pasta_curves::{
 use pasta_msm::utils::{gen_scalars, CommitmentKey};
 
 use plotters::prelude::*;
-use rand::RngCore;
+use rayon::{iter::{IntoParallelRefIterator, ParallelIterator}, slice::ParallelSliceMut};    
+use std::fs::OpenOptions;
+use std::io::BufReader;
 
 #[cfg(feature = "cuda")]
 extern "C" {
@@ -55,14 +57,12 @@ fn plot_scalars(
 
     // To avoid the IO failure being ignored silently, we manually call the present function
     root.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
-    println!("Result has been saved to {}", out_file);
+    eprintln!("Result has been saved to {}", out_file);
 
     Ok(())
 }
 
 fn read_abomonated<T: Abomonation + Clone>(name: String) -> std::io::Result<T> {
-    use std::fs::OpenOptions;
-    use std::io::BufReader;
 
     let arecibo = home::home_dir().unwrap().join(".arecibo_witness");
 
@@ -78,6 +78,70 @@ fn read_abomonated<T: Abomonation + Clone>(name: String) -> std::io::Result<T> {
     let (data, _) = unsafe { abomonation::decode::<T>(&mut bytes).unwrap() };
 
     Ok(data.clone())
+}
+
+struct Stats {
+    count: usize,
+    total: usize,
+}
+
+fn write_stats(scalars: &[pallas::Scalar], out_file: &str) -> std::io::Result<()> {
+    let stats = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(out_file)?;
+
+    let mut writer = BufWriter::new(stats);
+    let mut hist: HashMap<BigInt, usize> = HashMap::new();
+
+    scalars.iter().for_each(|x| {
+        let x = format!("{:?}", x);
+        let num = BigInt::from_str_radix(&x[2..], 16).unwrap();
+        *hist.entry(num).or_insert(0) += 1;
+    });
+
+    let mut entries: Vec<_> = hist.par_iter().collect();
+    entries.par_sort_by(|a, b| b.1.cmp(a.1));
+
+    let mut stats_100 = Stats { count: 0, total: 0};
+    let mut stats_500 = Stats { count: 0, total: 0};
+    let mut stats_1000 = Stats { count: 0, total: 0};
+    for (_, value) in entries.iter() {
+        if **value > 1000 {
+            stats_1000.count += 1;
+            stats_1000.total += *value;
+        } 
+        if **value > 500 {
+            stats_500.count += 1;
+            stats_500.total += *value;
+        } 
+        if **value > 100 {
+            stats_100.count += 1;
+            stats_100.total += *value;
+        } 
+    }
+
+    writeln!(&mut writer, "scalars length: {}", scalars.len())?;
+    let p1000 = 100.0 * (stats_1000.total as f64 / scalars.len() as f64);
+    let p500 = 100.0 * (stats_500.total as f64 / scalars.len() as f64);
+    let p100 = 100.0 * (stats_100.total as f64 / scalars.len() as f64);
+    writeln!(&mut writer, "freq>1000 (count, total, %): {:>10}, {:>10}, {:>10.2}%", stats_1000.count, stats_1000.total, p1000)?;
+    writeln!(&mut writer, "freq>500  (count, total, %): {:>10}, {:>10}, {:>10.2}%", stats_500.count, stats_500.total, p500)?;
+    writeln!(&mut writer, "freq>100  (count, total, %): {:>10}, {:>10}, {:>10.2}%", stats_100.count, stats_100.total, p100)?;
+    writeln!(&mut writer, "")?;
+    writeln!(&mut writer, "top 100 values:")?;
+
+    let mut count = 0;
+    // Print sorted (key, value) pairs
+    for (key, value) in entries {
+        if count < 100 {
+            writeln!(&mut writer, "{:>2}, {:>64}, {:?}", count, key.to_str_radix(16), value)?;
+        }
+        count += 1;
+    }
+    writer.flush()?;
+    Ok(())
 }
 
 /// cargo run --release --example abomonated
@@ -102,27 +166,12 @@ fn main() {
         if witness_n < 1_000_000 {
             continue;
         }
-
-        // println!("witness_n: {}", witness_n);
-
-        // let mut total = BigInt::zero();
-        // let mut dist: HashMap<BigInt, usize> = HashMap::new();
-        // // let mut small_count = vec![0; 10];
-        // for j in 0..witness_n {
-        //     let wj = format!("{:?}", witness_i[j]);
-        //     let num = BigInt::from_str_radix(&wj[2..], 16).unwrap();
-        //     total += &num;
-        //     *dist.entry(num).or_insert(0) += 1;
-        // }
-    
-        // let average = total / witness_n;
-        // println!("avg: {:?}", average);
-        // println!("dist len: {:?}", dist.len());
-        // // println!("smalls: {:?}", small_count);
         
-        let out_file = format!("plots/lurkrs_{i}.png");
-        plot_scalars(&witness_i, &out_file).unwrap();
+        // let out_file = format!("plots/lurkrs_{i}.png");
+        // plot_scalars(&witness_i, &out_file).unwrap();
+        let out_stats = format!("stats/{i}.txt");
+        write_stats(&witness_i, &out_stats).unwrap();
 
-        println!("done {i}\n");
+        println!("done {i}");
     }
 }
